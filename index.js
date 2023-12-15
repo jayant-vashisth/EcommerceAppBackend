714ddf129674d2c44676a065a47a950eea96873b
jayant-vashisth <jayant.relish@gmail.com>
2023-12-15 18:58:02 +0530

    product api's created

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const productRouter = require("./routes/Products");
+
+const port = 3000;
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use("/api/products", productRouter);
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(process.env.PORT || port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
